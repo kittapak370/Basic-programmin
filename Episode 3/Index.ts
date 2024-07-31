b8e5d45e3840f53a3802f1ee8d1f51d857b202fd
kittapak370 <kittapak09@gmail.com>
2024-08-01 01:34:17 +0700

    Update Index.ts

--- Episode 3/Index.ts
+++ Episode 3/Index.ts
@@ -1,1 +1,40 @@
- 
+function helloworld() {
+
+  console.log ("สวัสดีครับ")
+}
+
+helloworld ()
+
+function helloworld(name: string) {
+  console.log (name)
+}
+
+helloworld ('punn')
+
+function getPi() {
+  return 3.14 
+}
+console.log(getPi())
+
+
+
+function st(bname: string, cname: string) {
+
+  if (bname === 'mix' && cname === 'zane'){
+    console.log ('เริ่มการสอน')
+  } else {
+    console.log ('ไม่สอน')
+  }
+}
+
+st ('mix', 'zane')
+
+
+function st(dname: string, ename: string, fname: string) {
+  
+  if (!(fname === 'mix' ename === 'zane' fname === 'punn')) {
+    console.log ('พร้อมเรียน')
+  } else {
+    console.log ('ยังไม่พร้อม
+  }
+}
