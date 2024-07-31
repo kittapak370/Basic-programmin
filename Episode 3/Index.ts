d78eb76a0f5a2eba8a14c0c1d14e671d1c0465b3
kittapak370 <kittapak09@gmail.com>
2024-08-01 01:48:48 +0700

    Update Index.ts

--- Episode 3/Index.ts
+++ Episode 3/Index.ts
@@ -35,6 +35,25 @@ function st(dname: string, ename: string, fname: string) {
   if (!(fname === 'mix' ename === 'zane' fname === 'punn')) {
     console.log ('พร้อมเรียน')
   } else {
-    console.log ('ยังไม่พร้อม
+    console.log ('ยังไม่พร้อม')
   }
 }
+
+st ('mix', 'zane', 'punn')
+
+function bmi(height: number, weight: number) {
+  let score: number = weight / (height + height)
+  
+  if (score < 18.50){
+    return 'ผอม'
+  } else if (score < 22.90) {
+    return 'ปกติ'
+  } else if (score < 24.90) {
+    return 'อวบ'
+  } else if (score < 29.90) {
+    return 'อวบขึ้น'
+  } else {
+    return 'อวบมาก'
+  }
+}
+console.log (bmi(2.19,56) ) // การบ้าน BMI
