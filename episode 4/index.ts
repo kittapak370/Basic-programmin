010ec42cae5faf5806cecd11cf8c727dabc1a1bb
kittapak370 <kittapak09@gmail.com>
2024-08-02 16:45:09 +0700

    การบ้าน

--- episode 4/index.ts
+++ episode 4/index.ts
@@ -1,1 +1,39 @@
+function size(Size: string) {
+    switch (Size) {
+        case ('S'):
+            console.log ('ผอม')
+            break
+        case ('M'):
+            console.log ('ผอม')
+            break
+        case ('L'):
+            console.log ('ท้วม')
+            break
+        case ('XL'):
+            console.log ('อ้วน')
+            break
+        case ('2XL'):
+            console.log ('อ้วน')
+            break
+            
+            default: 
+            console.log ('กกกกกก')
+            break
+    }
+}
 
+function Manage(a: number) {
+    const x = a.toLocaleString()
+    return x
+}
+console.log(Manage(30000))
+
+function mfane (sto: string, indexs: string) {
+    if (sto.includes(indexs)) {
+        console.log(sto.indexOf(indexs))
+    } else {
+        console.log('ไม่พบคำที่ค้นหา')
+    }
+}
+
+mfane ('อยากนอนไหม', 'นอน')
