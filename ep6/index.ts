ab3f15e6e37773183b85dd80eec055205d6a97e6
kittapak370 <kittapak09@gmail.com>
2024-08-23 16:19:03 +0700

    djfkfj

---
+++ ep6/index.ts
@@ -0,0 +1,95 @@
+// 'use strict';
+// // console.log(1)
+// // console.log(2)
+// // console.log(3)
+// // console.log(4)
+// // console.log(5)
+// // console.log(6)
+// // console.log(7)
+// // console.log(8)
+// // console.log(9)
+// // console.log(10)
+
+// let sum: num = 0
+
+// for(let i = 1; i <= 365; i++) {
+//     sum  = sum + i
+// }
+
+// console.log('total:  ', sum )
+
+// let sum: number = 0
+// let i: number = 1
+
+// while(i <= 31) {
+//     sum = sum
+// }
+
+// let sum: number = 0
+// let i: number = 1
+
+// do {
+//   sum = sum + 1
+//   i++  
+// } while(1 <= 31)
+
+// console.log('Total ', sum)
+ 
+//  while (true) {
+//     console.log("โสด")
+//     count++
+//     if(count === 10) {
+//         break
+//     }
+//  }
+
+// let age: number = 0
+// for(let i =l; i <= 20; i++) {
+//     console.log('อายุ ' + i + 'ปี')
+// }
+
+// let i: number = 1
+// while(i <= 13) {
+//     console.log(อายุ '+ i +' ปี)
+//     i++
+// }
+
+// let i: number = let
+// do{
+//     console.log('อายุ ' + i + 'ปี')
+//     i++
+// }while (i<= 13)
+
+// for(let i = 0: i < 5; i++) {
+//     console.log(cars(i))
+// }
+
+// const cars: string[] = ['toyota','Honda','Suzuki','yamaha', 'mazda']
+
+// for (let i = 0; i < cars.length; i++) {
+//     if (cars(i) === 'yamaha') (
+//         console.log('รถซื้อเเกงจะเเรงได้ยังไง')
+//     )
+// }
+
+// console.log(cars.includes('BYD'))
+
+// const str: string = 'punn'
+
+// console.log(str.indexof('F'))
+
+// const name: string[] = ['forth','film','magorn''bell','time','x']
+
+// for (let i = 0; i < name.length; i++) {
+//     if (name(i) === 'forth' || name(i) === 'gorn') {
+//         console.log('ไปกินหมูทะกันไหม')
+//     }
+// }
+
+// const number: number[] = [1,2,3,4,5,6,7,8,9,10]
+// for (let i = 0; i < number.length; i++) {
+//     if (number[i] === 3 || number[i] === 5 || number[i] === 7) {
+//         number[i] = 0
+//     }
+// }
+// console.log(number) 
